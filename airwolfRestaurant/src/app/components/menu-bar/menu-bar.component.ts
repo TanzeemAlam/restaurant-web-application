b989c9eaf8b6f9810db401cfb48edb8576aa1346
TanzeemAlam <tanzeemalam789@gmail.com>
2020-10-23 17:02:38 +0530

    Added a service layer to fetch json data using api

--- airwolfRestaurant/src/app/components/menu-bar/menu-bar.component.ts
+++ airwolfRestaurant/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceComponent } from '../../services/service.component';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
-  constructor() { }
+  public title: String = "Menu Bar";
+  public snackItems: any [] = ['One','Two','Three'];
+  public mainCourseItems: any [] = ['One','Two','Three'];
+  public sweetItems: any [] = ['One','Two','Three'];
+
+  constructor(private httpService: ServiceComponent) { }
 
   ngOnInit(): void {
+    this.httpService.getMenuData();
   }
 
 }
