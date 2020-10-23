5c83b0c5bd5e7dbfc38c96b6cc1a14c4b9640269
alisha <alisha@nagarro.com>
2020-10-23 12:21:53 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+URLs to access this Web API:
+
+https://localhost:44324/api/restaurantmenu/listfiles
+
+https://localhost:44324/api/restaurantmenu/downloadfile/<fileName>
---
+++ RestaurantWebApplication/Controllers/RestaurantMenuController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RestaurantWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantMenuController : ControllerBase
+    {
+        private readonly IOptions<MyConfig> config;
+
+
+        public RestaurantMenuController(IOptions<MyConfig> config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet("ListFiles")]
+        public async Task<List<string>> ListFiles()
+        {
+            List<string> blobs = new List<string>();
+            try
+            {
+                if (CloudStorageAccount.TryParse(config.Value.StorageConnection, out CloudStorageAccount storageAccount))
+                {
+                    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                    CloudBlobContainer container = blobClient.GetContainerReference(config.Value.Container);
+
+                    BlobResultSegment resultSegment = await container.ListBlobsSegmentedAsync(null);
+                    foreach (IListBlobItem item in resultSegment.Results)
+                    {
+                        if (item.GetType() == typeof(CloudBlockBlob))
+                        {
+                            CloudBlockBlob blob = (CloudBlockBlob)item;
+                            blobs.Add(blob.Name);
+                        }
+                        else if (item.GetType() == typeof(CloudPageBlob))
+                        {
+                            CloudPageBlob blob = (CloudPageBlob)item;
+                            blobs.Add(blob.Name);
+                        }
+                        else if (item.GetType() == typeof(CloudBlobDirectory))
+                        {
+                            CloudBlobDirectory dir = (CloudBlobDirectory)item;
+                            blobs.Add(dir.Uri.ToString());
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return blobs;
+        }
+
+        [HttpGet("DownloadFile/{fileName}")]
+        public async Task<IActionResult> DownloadFile(string fileName)
+        {
+            MemoryStream ms = new MemoryStream();
+            if (CloudStorageAccount.TryParse(config.Value.StorageConnection, out CloudStorageAccount storageAccount))
+            {
+                CloudBlobClient BlobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = BlobClient.GetContainerReference(config.Value.Container);
+
+                if (await container.ExistsAsync())
+                {
+                    CloudBlob file = container.GetBlobReference(fileName);
+
+                    if (await file.ExistsAsync())
+                    {
+                        await file.DownloadToStreamAsync(ms);
+                        Stream blobStream = file.OpenReadAsync().Result;
+                        return File(blobStream, file.Properties.ContentType, file.Name);
+                    }
+                    else
+                    {
+                        return Content("File does not exist");
+                    }
+                }
+                else
+                {
+                    return Content("Container does not exist");
+                }
+            }
+            else
+            {
+                return Content("Error opening storage");
+            }
+        }
+    }
+}
